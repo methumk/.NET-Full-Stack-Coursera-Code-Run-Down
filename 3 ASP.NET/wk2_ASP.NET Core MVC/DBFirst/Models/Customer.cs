c8c7c60bbd5167476c3565ec66c023c443d45b67
methumk <methum.cg@gmail.com>
2024-02-26 17:56:43 -0800

    DB First approach

---
+++ 3 ASP.NET/wk2_ASP.NET Core MVC/DBFirst/Models/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DBFirst.Models;
+
+public partial class Customer
+{
+    public int CustomerId { get; set; }
+
+    public string? CustomerName { get; set; }
+
+    public double? CustomerAmount { get; set; }
+
+    public string? City { get; set; }
+}
---
+++ 3 ASP.NET/wk2_ASP.NET Core MVC/DBFirst/Models/DbfirstApproachContext.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace DBFirst.Models;
+
+public partial class DbfirstApproachContext : DbContext
+{
+    public DbfirstApproachContext()
+    {
+    }
+
+    public DbfirstApproachContext(DbContextOptions<DbfirstApproachContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Customer> Customers { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=DESKTOP-AJ3RPD9;Database=DBFirstApproach;Integrated Security=true;TrustServerCertificate=true;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Customer>(entity =>
+        {
+            entity.HasKey(e => e.CustomerId).HasName("PK__Customer__A4AE64B8608F5A3C");
+
+            entity.ToTable("Customer");
+
+            entity.Property(e => e.CustomerId).HasColumnName("CustomerID");
+            entity.Property(e => e.City)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.CustomerName)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}

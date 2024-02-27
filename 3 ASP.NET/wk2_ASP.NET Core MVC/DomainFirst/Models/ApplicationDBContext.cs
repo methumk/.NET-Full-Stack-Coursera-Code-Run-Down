25d8890b3c7807aed7164fb8457e52c2cda72ddf
methumk <methum.cg@gmail.com>
2024-02-26 17:21:39 -0800

    Domain First to DB Example

---
+++ 3 ASP.NET/wk2_ASP.NET Core MVC/DomainFirst/Models/ApplicationDBContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ORMApp.Models
+{
+/*
+Examples: ORM
+    - intellisense comes from Entity framework core nuget packages
+    - We want to access (and scaffold) the customer model from the DB so we have to add that as a variable
+ */
+    public class ApplicationDBContext : DbContext
+    {
+        // Make sure to pluralize the db from the model (i.e s and ies)
+        // Allows communication with the table from the DB through this attribute
+        public DbSet<Customer> Customers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // Override the default Onconfigure file from DbContext
+            // This should read from the optionsBuilder
+
+            // We are using the SQLServer db
+            // Then we provide connection string
+            // Database is your database name you want to create/add to?
+            // Server name can be . to access your local DB server or you can get the name from the SQL Server management studio
+            optionsBuilder.UseSqlServer("Server=DESKTOP-AJ3RPD9;Database=MyDB;Integrated Security=true;TrustServerCertificate=true;");
+            base.OnConfiguring(optionsBuilder);
+        }
+
+    }
+}
---
+++ 3 ASP.NET/wk2_ASP.NET Core MVC/DomainFirst/Models/Customer.cs
@@ -0,0 +1,17 @@
+﻿namespace ORMApp.Models
+{
+    /* 
+    Examples: ORM
+        - Step 1
+        - This is code first approach (create model first -> migrate to DB -> manage with EF Core)
+        - This is our customer which has fields that will show up in our DB table
+     */
+    public class Customer
+    {
+        // NOTE: field with <modelName>Id or Id will become primary key
+        // NOTE: Every entity should have Primary key column
+        public int CustomerId { get; set; }
+        public string? CustomerName { get; set; }
+        public double CustomerAmount { get; set; }
+    }
+}
